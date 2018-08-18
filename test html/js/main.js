2e830b76335382ddb041c304a540f1c59c4e9085
Serg <sergik148@gmail.com>
2018-08-18 23:28:32 +0300

    test
    test

--- test html/js/main.js
+++ test html/js/main.js
@@ -1,12 +1,16 @@
-//btn_top
- window.onscroll = function() {
-   var scrollElem = document.getElementById("scrollToTop");
-   if (document.body.scrollTop > document.documentElement.clientHeight) {
-      scrollElem.style.display = "1";
+var lastScrollTop = 0;
+var scrollElem = document.getElementById("scrollToTop")
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+       scrollElem.style.opacity = "1";
    } else {
        scrollElem.style.opacity = "0";
    }
-}
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+ 
 var timeOut;
 function goUp() {
    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
@@ -23,5 +27,3 @@ function initMap() {
       document.getElementById('map'), {zoom: 12.37, center: uluru});
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
-
-
