43640287bf7857101fc5c361e76f63a8c92e88f2
Serg <sergik148@gmail.com>
2019-03-01 05:10:22 +0300

    work_27_02_19 Add accordion
    Add accordion

--- test_html/test_work/work_27_02_19/js/main.js
+++ test_html/test_work/work_27_02_19/js/main.js
@@ -1,3 +1,19 @@
+// ==== Accordion  ====
+
+ var title = document.getElementsByClassName('Arcadion_item');
+for (var i = 0; i < title.length; i++) {
+    title[i].addEventListener('click', function() {
+        if (!(this.classList.contains('Arcadion_item-active'))) {
+            for(var i = 0; i < title.length; i++) {
+                title[i].classList.remove('Arcadion_item-active');
+            }
+            this.classList.add('Arcadion_item-active');
+        }
+    })
+}
+  
+// === Modal ===
+
   document.getElementById('btn-modal').addEventListener('click', function() {
   	document.getElementById('overlay').classList.add('is-visible');
   	document.getElementById('modal').classList.add('is-visible');
@@ -11,5 +27,7 @@
   	document.getElementById('overlay').classList.remove('is-visible');
   	document.getElementById('modal').classList.remove('is-visible');
   });
+
+
 
 
