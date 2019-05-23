d6f362a0716fdb6858a578a2a986c4c430f8cffa
Serg <sergik148@gmail.com>
2019-05-23 22:06:08 +0300

    Project_X(boostrap)(2h)
    (boostrap)(2h)

---
+++ test_html/test_work/project_X(boostrap)(2h)/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#ExamplesOfOurWorkCarousel").owlCarousel({
+    items : 3,
+    nav : true,
+    dots : false
+  });
+  $("#ProductsInStockCarousel").owlCarousel({
+    items : 4,
+    nav : true,
+    dots : true
+  });
+  $(".owl-carousel").owlCarousel({
+    items : 1,
+    nav : true,
+    dots : true
+  });
+  
+});
+
+
