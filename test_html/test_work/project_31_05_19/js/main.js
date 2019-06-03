d0f3f1823731fe8d299c2c39ec50b037eb756f79
Serg <sergik148@gmail.com>
2019-06-03 19:10:32 +0300

    add project_31_05_19
    basic site grid

---
+++ test_html/test_work/project_31_05_19/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#AboutUs__Carousel").owlCarousel({
+    nav : false,
+    dots : true,
+    loop : true,
+    items: 1
+  });
+  $("#WhatTheySays__Carousel").owlCarousel({
+    nav : true,
+    dots : true,
+    loop : true,
+    items: 1
+  });
+
+});
+
+
